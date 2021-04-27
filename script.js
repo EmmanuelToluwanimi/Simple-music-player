48279e31b207541d6c9dc21bacd74413d896a35b
EmmanuelToluwanimi <emmanueltoluwanimi01@gmail.com>
2021-04-27 19:20:55 +0100

    songs playing

--- script.js
+++ script.js
@@ -7,14 +7,65 @@ let nextsongbtn = document.querySelector("#nextbtn");
 let prevsongbtn = document.querySelector("#prevbtn");
 let volbtn = document.querySelector("#volbtn");
 let voltoggle = document.querySelector(".fa-volume-up");
-let volume_range= document.querySelector("#inp_volume");
+let volume_range = document.querySelector("#inp_volume");
+let artistname = document.querySelector('#artist-name');
+let titlename = document.querySelector('#title-name');
+let albumimage = document.querySelectorAll('.albumimg');
+let track = document.createElement('audio');
 
+let songplaying = false;
+let songlist;
+
+// function to fetch songs
+function fetchsongs() {
+    fetch('./albumsongs.json')
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data);
+            album_details(data);
+            songlist = data.songs;
+        })
+}
+fetchsongs();
+
+// functions to display album details
+function album_details(album) {
+    console.log(album);
+    artistname.innerHTML = album.artistname;
+    titlename.innerHTML = album.songs[0].songname;
+    albumimage.forEach(e => {
+        e.src = album.albumimage;
+    });
+}
 
 // functions on play button
-playbtn.addEventListener("click", play)
+playbtn.addEventListener('click', play)
 function play() {
-    console.log(playtoggle);
+    // console.log(songlist[0]);
+    track.src = songlist[0].src;
+    
+    if (track.canPlayType) {
+        // console.log("true");
+        if (songplaying == false) {
+            console.log(songplaying);
+            plays();
+        } else {
+            console.log(songplaying);
+            pauses();
+        }
+    }else{
+        console.log("Song cannnot be played");
+    }
     togplaybtn();
+}
+
+function plays() {
+    songplaying = true;
+    track.play();
+}
+function pauses() {
+    songplaying = false;
+    track.pause();
 }
 
 // function to toggle play and pause icon
@@ -24,7 +75,7 @@ function togplaybtn() {
 }
 
 // function on shuffle button
-shufflebtn.addEventListener("click",shufflesong);
+shufflebtn.addEventListener("click", shufflesong);
 function shufflesong() {
     shuffletog();
 }
@@ -38,19 +89,23 @@ function shuffletog() {
 volbtn.addEventListener('click', volumebtn)
 function volumebtn() {
     mutevol();
-    
+
 }
 
 function mutevol() {
     voltoggle.classList.toggle('fa-volume-mute');
     voltoggle.classList.toggle('fa-volume-up');
 
-    volume_range.value= 0;
-    // stopped here
+    volume_range.value = 0;
+
 }
 
-volume_range.addEventListener('click', volume_change)
+// function to change volume
+volume_range.addEventListener('input', volume_change)
 function volume_change() {
-    
-    console.log(volume_range.value);
+    console.log(this.value);
 }
+
+
+
+
